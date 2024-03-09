6ee66a50322f48b9a16a6dc0cd62dd3c7ee12f54
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-09 21:41:57 +0900

    feat: GetAudio

--- Assets/02_Scripts/AudioManage/AudioPackage.cs
+++ Assets/02_Scripts/AudioManage/AudioPackage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace AudioManage
@@ -11,6 +12,36 @@ namespace AudioManage
         public string packageName;
         [Space(10)]
         public List<AudioCell> audioCells;
+        
+        
+        [CanBeNull]
+        public AudioCell GetAudio(int id)
+        {
+            for (int i = 0; i < audioCells.Count; i++)
+            {
+                if (id == audioCells[i].id)
+                {
+                    return audioCells[i];
+                }   
+            }
 
+            return null;
+        }
+        
+        [CanBeNull]
+        public AudioCell GetAudio(string name)
+        {
+            for (int i = 0; i < audioCells.Count; i++)
+            {
+                if (name == audioCells[i].name)
+                {
+                    return audioCells[i];
+                }   
+            }
+
+            return null;
+        }
+
+        
     }
 }
