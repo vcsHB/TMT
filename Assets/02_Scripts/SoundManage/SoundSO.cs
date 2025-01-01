e769d1d9367234fbedcb5542872fe780fd608eed
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-01-01 14:36:25 +0900

    feat: Version Up
    Unity 6000.0.31f1

---
+++ Assets/02_Scripts/SoundManage/SoundSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MINISound
+{
+
+
+    [CreateAssetMenu(menuName = "TMT/SO/SoundSO")]
+    public class SoundSO : ScriptableObject
+    {
+        
+    }
+
+}
